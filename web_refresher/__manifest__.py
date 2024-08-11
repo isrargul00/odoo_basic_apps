ee08b7d8ac9c38346d55db78ce9c769b8af94f24
israr <isrargul00@gmail.com>
2024-08-12 03:30:12 +0500

    done

---
+++ web_refresher/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Web Refresher",
+    "version": "17.0.1.1.0",
+    "author": "Compassion Switzerland, Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "installable": True,
+    "auto_install": False,
+    "assets": {
+        "web.assets_backend": [
+            "web_refresher/static/src/scss/refresher.scss",
+            "web_refresher/static/src/js/refresher.esm.js",
+            "web_refresher/static/src/js/control_panel.esm.js",
+            "web_refresher/static/src/xml/refresher.xml",
+            # Load the modification of the master template just after it,
+            # for having the modification in all the primary extensions.
+            # Example: the project primary view.
+            (
+                "after",
+                "web/static/src/search/control_panel/control_panel.xml",
+                "web_refresher/static/src/xml/control_panel.xml",
+            ),
+        ],
+    },
+}
